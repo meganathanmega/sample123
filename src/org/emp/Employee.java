77356743f1e99be16f9a79097e6663d693db6f69
meganathanmega <meganathan29mech@gmail.com>
2022-07-20 23:13:58 +0530

    A branch is updated

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -8,16 +8,16 @@ public class Employee {
 		System.out.println("kodamkkam");
 	}
 	private void empDob() {
-		System.out.println("29/05/1989");
+		System.out.println("2/01/89");
 	}
 	private void empPhone() {
-		System.out.println("90472716");
+		System.out.println("72716");
 	}
 	private void empEmail() {
 		System.out.println("mega@gmail.com");
 	}
 	private void empAddress() {
-		System.out.println("chennai");
+		System.out.println("nnai");
 	}
 
 	public static void main(String[] args) {
